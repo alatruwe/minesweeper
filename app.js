b68307889c6320d37d2f698a9e22403362a440e8
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-07-01 13:44:13 -0700

    created game array with random bombs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let width = 10;
+  let bombAmount = 20;
   let squares = [];
 
   // create board
   function createBoard() {
+    // get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill("bomb");
+    const emptyArray = Array(width * width - bombAmount).fill("valid");
+    const gameArray = emptyArray.concat(bombsArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
     for (let i = 0; i < width * width; i++) {
       // create square
       const square = document.createElement("div");
@@ -12,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
       square.setAttribute("id", i);
       // add it to grid
       grid.appendChild(square);
-      //
       squares.push(square);
     }
   }
